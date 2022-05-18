5c2264dcc076f9943917f8323a698d281b100825
kenanmuradli <muradlikenan2001@gmail.com>
2022-05-19 02:16:22 +0400

    Write quick sort algorithms

--- src/com/company/sorts/QuickSort.java
+++ src/com/company/sorts/QuickSort.java
@@ -76,8 +76,8 @@ public class QuickSort {
     }
 
     private static void printArray(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }
