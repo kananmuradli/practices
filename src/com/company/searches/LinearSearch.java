17d8e40a35a86ca7418739d630a4de88c3bc67ad
kanan.muradli <muradlikenan2001@gmail.com>
2022-08-01 12:10:03 +0400

    control input data

--- src/com/company/searches/LinearSearch.java
+++ src/com/company/searches/LinearSearch.java
@@ -1,6 +1,13 @@
 package com.company.searches;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LinearSearch {
+
+
 
     public static int linearSearch(int[] arr, int x) {
         int n = arr.length;
@@ -22,5 +29,45 @@ public class LinearSearch {
             System.out.println("Element is not present in array");
         else
             System.out.println("Element is present at index - " + result);
+    }
+}
+
+class NK {
+    public static void main(String[] args) {
+        System.out.println(controlInputData1("Kenan^"));
+    }
+
+    public static String controlInputData1(String str) {
+        String regex = "[^A-Za-z0-9]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.replaceAll("");
+    }
+}
+
+class Prt {
+    private static ArrayList<Character> special = new ArrayList<>(Arrays.asList('&', '$', '@', '#', '^'));
+
+    public static void main(String[] args) {
+        String test = "05-06.07.2022-ci il tarixdə & HYDROVIP GROUP & MMC tərəfindən elektron qaimə-faktura məlumatlarına $ əsasən cəmi 41111,2 manat ";
+        String outputText = "";
+
+        for (int i = 0; i < test.length(); i++) {
+            char c = test.charAt(i);
+            if (!special.contains(c))
+                outputText += c;
+            else if (c == '&') {
+                outputText += "and";
+            } else if (c == '$') {
+                outputText += "dollar";
+            } else if (c == '@') {
+                outputText += " ";
+            } else if (c == '#') {
+                outputText += " ";
+            } else if (c == '^') {
+                outputText += " ";
+            }
+        }
+        System.out.println(outputText);
     }
 }
