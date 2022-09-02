097abb09e69defe5b475624a76cde85135959ac7
kanan.muradli <muradlikenan2001@gmail.com>
2022-09-02 16:02:46 +0400

    solving divide two integer algorithms without  multiplication, division, and mod operator.(some changes)

--- src/com/company/leetcode/DivideTwoIntegers.java
+++ src/com/company/leetcode/DivideTwoIntegers.java
@@ -3,26 +3,34 @@ package com.company.leetcode;
 public class DivideTwoIntegers {
 
     public static void main(String[] args) {
-        System.out.println(divideWithWhileLoop(23, 3));
-        System.out.println(divideWithForLoop(23, 3));
+        System.out.println(divideWithWhileLoop(7, -2));
+        System.out.println(divideWithForLoop(7, -2));
     }
 
     public static int divideWithWhileLoop(int dividend, int divisor) {
+        int sign = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? -1 : 1;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+
         int result = 0;
         while ((dividend - divisor) >= 0) {
             result++;
             dividend = dividend - divisor;
         }
-        return result;
+        return sign < 0 ? -result : result;
     }
 
     public static int divideWithForLoop(int dividend1, int divisor1) {
+        int sign1 = (dividend1 < 0 && divisor1 > 0) || (dividend1 > 0 && divisor1 < 0) ? -1 : 1;
+        dividend1 = Math.abs(dividend1);
+        divisor1 = Math.abs(divisor1);
+
         int result1 = 0;
         for (int i = 0; (dividend1 - divisor1) >= 0; i++) {
             result1++;
             dividend1 = dividend1 - divisor1;
         }
-        return result1;
+        return sign1 < 0 ? -result1 : result1;
     }
 }
 
