9c29c0b8508f5cc25f4b6d01d7a6594e39524494
kenanmuradli <muradlikenan2001@gmail.com>
2022-09-03 17:20:02 +0400

    leet code tow divide integer solution

--- src/com/company/leetcode/DivideTwoIntegers.java
+++ src/com/company/leetcode/DivideTwoIntegers.java
@@ -9,7 +9,9 @@ public class DivideTwoIntegers {
         System.out.println(divideWithWhileLoop(-7, -2));
         System.out.println(divideWithWhileLoop(7, 1));
         System.out.println(divideWithWhileLoop(7, -1));
+
         System.out.println();
+
         System.out.println(divideWithForLoop(7, -2));
         System.out.println(divideWithForLoop(-7, 2));
         System.out.println(divideWithForLoop(7, 2));
@@ -19,11 +21,8 @@ public class DivideTwoIntegers {
     }
 
     public static int divideWithWhileLoop(int dividend, int divisor) {
-        if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
-        if (dividend == Integer.MIN_VALUE && divisor == 1) return Integer.MIN_VALUE;
-        if (dividend == divisor) return 1;
-        if (divisor == 1) return dividend;
-        if (divisor == -1) return -dividend;
+        Integer maxValue = getControl(dividend, divisor);
+        if (maxValue != null) return maxValue;
 
         int sign = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? -1 : 1;
         dividend = Math.abs(dividend);
@@ -38,24 +37,30 @@ public class DivideTwoIntegers {
         return sign < 0 ? -result : result;
     }
 
-    public static int divideWithForLoop(int dividend1, int divisor1) {
-        if (dividend1 == Integer.MIN_VALUE && divisor1 == -1) return Integer.MAX_VALUE;
-        if (dividend1 == Integer.MIN_VALUE && divisor1 == 1) return Integer.MIN_VALUE;
-        if (dividend1 == divisor1) return 1;
-        if (divisor1 == 1) return dividend1;
-        if (divisor1 == -1) return -dividend1;
+    public static int divideWithForLoop(int dividend, int divisor) {
+        Integer maxValue = getControl(dividend, divisor);
+        if (maxValue != null) return maxValue;
 
-        int sign1 = (dividend1 < 0 && divisor1 > 0) || (dividend1 > 0 && divisor1 < 0) ? -1 : 1;
-        dividend1 = Math.abs(dividend1);
-        divisor1 = Math.abs(divisor1);
-        if (dividend1 == divisor1) return -1;
+        int sign1 = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? -1 : 1;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        if (dividend == divisor) return -1;
 
         int result1 = 0;
-        for (int i = 0; (dividend1 - divisor1) >= 0; i++) {
+        for (int i = 0; (dividend - divisor) >= 0; i++) {
             result1++;
-            dividend1 = dividend1 - divisor1;
+            dividend = dividend - divisor;
         }
         return sign1 < 0 ? -result1 : result1;
+    }
+
+    private static Integer getControl(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+        if (dividend == Integer.MIN_VALUE && divisor == 1) return Integer.MIN_VALUE;
+        if (dividend == divisor) return 1;
+        if (divisor == 1) return dividend;
+        if (divisor == -1) return -dividend;
+        return null;
     }
 }
 
