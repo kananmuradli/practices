b4f64820f5d07d1275ad6e613741163385a6b9a0
kanan.muradli <muradlikenan2001@gmail.com>
2023-06-18 00:21:47 +0400

    init

--- src/com/company/leetcode/TwoSum.java
+++ src/com/company/leetcode/TwoSum.java
@@ -39,7 +39,7 @@ public class TwoSum {
   }
 
   public static void main(String[] args) {
-    int[]nums = {2,7,11,15};
+    int[] nums = {2, 11, 7, 15};
     int target = 26;
     System.out.println(Arrays.toString(twoSum(nums, target)));
   }
