8bd0cdc4063e1a98df3f9aa65486ce75e5717247
kenanmuradli <muradlikenan2001@gmail.com>
2023-12-10 00:35:19 +0400

    practices solid principles with code examples

--- src/com/company/patterns/solid/s/Order.java
+++ src/com/company/patterns/solid/s/Order.java
@@ -1,5 +1,8 @@
 package com.company.patterns.solid.s;
 
+//this principle states that each class should have one responsibility, one single purpose.
+// this means that a class will do only one job.
+//in our class we do not write any additional methods or variables.
 public class Order {
     private final Long id;
     private final String address;
