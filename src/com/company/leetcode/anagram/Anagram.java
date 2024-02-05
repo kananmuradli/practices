7abd65bc3fac23d80ae006034ca29d7238e05c2d
kenanmuradli <muradlikenan2001@gmail.com>
2024-02-05 13:16:48 +0400

    solve anagram algorithm

---
+++ src/com/company/leetcode/anagram/Anagram.java
@@ -0,0 +1,92 @@
+package com.company.leetcode.anagram;
+
+/*
+Writing Java code for checking word is anagram
+(a word or phrase made by transposing the letters of another word or phrase) or not
+-> input race,care -> output true
+input -> test, code -> output false
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        String a = "mala", b = "alma";
+        System.out.println(isAnagramSort(a, b));
+        System.out.println(isAnagram(a, b));
+        System.out.println(foo('B'));
+    }
+
+    public static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        var str = new ArrayList<String>();
+        for (int i = 0; i < a.length(); i++) {
+            char c = a.charAt(i);
+            str.add(c + "");
+        }
+
+        for (int j = 0; j < b.length(); j++) {
+
+            if (Character.isUpperCase(b.charAt(j))) {
+                char d = Character.toLowerCase(b.charAt(j));
+                str.remove(d + "");
+                if (Character.isLowerCase(d)) {
+                    char d1 = Character.toUpperCase(b.charAt(j));
+                    str.remove(d1 + "");
+                }
+            }
+
+            if (Character.isLowerCase(b.charAt(j))) {
+                char d = Character.toUpperCase(b.charAt(j));
+                str.remove(d + "");
+                if (Character.isUpperCase(d)) {
+                    char d1 = Character.toLowerCase(b.charAt(j));
+                    str.remove(d1 + "");
+                }
+            }
+        }
+
+        if (str.size() == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isAnagramSort(String a, String b) {
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        var a1 = a.toCharArray();
+        var a2 = b.toCharArray();
+
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+
+        return Arrays.equals(a1, a2);
+    }
+
+    public static char foo(char a) {
+        if (a >= 65 && a <= 90) {
+            a += 32;
+            return a;
+        }
+        if (a >= 97 && a <= 122) {
+            a-=32;
+            return a;
+        }
+        return '*';
+    }
+}
+
+/*
+Write Java code for finding numbers of maximum element in array -> input [4,1,5,5,1,5] -> output 3
+Write Java code for finding factorial of number -> input 3 -> 6
+*/
